052998a9de22a4abbbdad61231c488fb48fb26cd
Colin Richardson <colin.r.richardson@me.com>
2024-01-23 09:46:10 -0500

    Cleanup

--- test.R
+++ test.R
@@ -3,12 +3,14 @@
 
 #git and rstudio integration: https://www.youtube.com/watch?v=B4b7Jmyxrxw&list=PLSviU861UtD81AuyYb3SbndmAA_qTCoLe&index=3
 
-install.packages("usethis")
 library("usethis")
 edit_git_config()
 
 
 
 use_git()
+
+
+#this is a new line 
 
 
