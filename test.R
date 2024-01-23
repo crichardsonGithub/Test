e0229c5c32b3cb0afd07e3fcf43200cb0b6443ea
Colin Richardson <colin.r.richardson@me.com>
2024-01-23 09:44:07 -0500

    Test cleanup

---
+++ test.R
@@ -0,0 +1,14 @@
+#this is a test of local config
+
+
+#git and rstudio integration: https://www.youtube.com/watch?v=B4b7Jmyxrxw&list=PLSviU861UtD81AuyYb3SbndmAA_qTCoLe&index=3
+
+install.packages("usethis")
+library("usethis")
+edit_git_config()
+
+
+
+use_git()
+
+
